a6122a572b1f470870cc3ded46bfefa20454fcec
Javier Jorge <JJorgeDSIC@users.noreply.github.com>
2026-02-14 19:59:32 +0100

    Rigidbody and callbacks

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,14 +8,49 @@ public class PlayerScript : MonoBehaviour
     private PlayerInput playerInput;
     private Animator animator;
 
+    //Atributo para la velocidad
+    [Header("Parameters")]
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+
+    //Vector2 para almacenar la variación del input
+    Vector2 inputMovement;
+
+    float deltaXMovement;
+    float deltaYMovement;
+
 
     void Start()
     {
-       
+        //Recuperamos los componentes que necesitamos
+        rb = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+        animator = GetComponent<Animator>();
+        inputMovement = Vector2.zero;
+        speed = 1f;
+        jumpForce = 4f;
     }
 
     void Update()
     {
+        //Obtenemos el movimiento en horizontal del componente PlayerInput
+        inputMovement = playerInput.actions["Move"].ReadValue<Vector2>();
+
+        deltaXMovement = speed * inputMovement.x;
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //Modificamos la velocidad que tiene el Rigidbody2D en cada actualización en base a la entrada
+        rb.linearVelocity = new Vector2(deltaXMovement, rb.linearVelocity.y);
+    }
+
+    //Callback para el salto
+    public void Jump(InputAction.CallbackContext callbackContext)
+    {
+        //Cuando se lanza este evento, se aplica una fuerza hacia arriba para simular el salto
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
 }
