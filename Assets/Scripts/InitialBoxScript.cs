fda72bb5e9430415f50675cb931f7112c0fd1e1b
jjorgeProjects <jjorge@florida-uni.es>
2026-03-05 22:36:01 +0100

    Initial box disappearing after 2 secs

--- Assets/Scripts/InitialBoxScript.cs
+++ Assets/Scripts/InitialBoxScript.cs
@@ -1,14 +1,34 @@
 using UnityEngine;
+using System.Collections;
 
 public class InitialBoxScript : MonoBehaviour
 {
     void Start()
     {
-        
+        //Varias opciones para hacerla desaparecer al iniciar la escena, puedes usar cualquiera de las siguientes:
+        // Opción 1: Destruir el GameObject
+        //Destroy(gameObject, 2f); --> Destruye el GameObject después de 2 segundos
+        // Opción 2:Llama al método DestroyBox después de 2 segundos
+        //Invoke("DestroyBox", 2f); 
+        // Opción 3: Coroutina para destruir el GameObject después de un tiempo
+        StartCoroutine(DestroyAfterTime(2f));
+
+
     }
 
     void Update()
     {
         
+    }
+
+    void DestoyBox()
+    {
+        Destroy(gameObject);
+    }
+
+    IEnumerator DestroyAfterTime(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
     }
 }
